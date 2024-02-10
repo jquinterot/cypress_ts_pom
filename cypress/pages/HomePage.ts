8ac4a755f0aee3f1213f028781f3097927822e67
Johany Quintero <johandquintero@utp.edu.co>
2024-02-09 19:26:00 -0500

    Add page object model

---
+++ cypress/pages/HomePage.ts
@@ -0,0 +1,9 @@
+class HomePage {
+
+   getNavBarTitle = ()=> cy.get('#nava').contains('PRODUCT STORE');
+   getCategoryItems = (category:string) => cy.contains(`${category}`);
+   getProduct = (product:string) =>   cy.contains(`${product}`);
+   getNavbarMenuOption = (menuOption:string) => cy.contains(`${menuOption}`);
+}
+
+export default HomePage;
